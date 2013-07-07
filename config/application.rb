e9bc5260b16bbb31c10a2c52a39848881fe8fc47
ruedap <ruedap@ruedap.com>
2013-07-08 05:16:42 +0900

    Set to .sass as default for stylesheets

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module Daplog
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Set to .sass as default for stylesheets
+    config.sass.preferred_syntax = :sass
   end
 end
