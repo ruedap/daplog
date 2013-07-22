9a7d6493af014b49821cf7b89bab0ee0f5b4a380
ruedap <ruedap@ruedap.com>
2013-07-23 05:21:59 +0900

    Fix raven.rb for test environment

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,13 +1,14 @@
-require 'raven'
+unless Rails.env.test?
+  require 'raven'
+  Raven.configure do |config|
+    config.dsn = ENV['SENTRY_DSN']
 
-Raven.configure do |config|
-  config.dsn = ENV['SENTRY_DSN']
+    # 例外通知を行う環境
+    config.environments = %w( production staging )
 
-  # 例外通知を行う環境
-  config.environments = %w( production staging )
-
-  # 例外通知を行わない環境の場合はログの出力を行わない
-  unless config.environments.include?(Rails.env)
-    config.logger = false
+    # 例外通知を行わない環境の場合はログの出力を行わない
+    unless config.environments.include?(Rails.env)
+      config.logger = false
+    end
   end
 end
