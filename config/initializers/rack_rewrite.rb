93e2a3d20948c3a01a3ef4fd869340a002bc1b5f
ruedap <ruedap@ruedap.com>
2013-07-23 07:12:11 +0900

    Redirect to blog.ruedap.com from herokuapp.com

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -1,3 +1,10 @@
 Daplog::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
   r301 %r{^/(.*)/$}, '/$1'
+
+  if ENV['RACK_ENV'] == 'production'
+    r301 %r{.*}, 'http://blog.ruedap.com$&', if: Proc.new {|rack_env|
+      rack_env['SERVER_NAME'] =~ /.+\.herokuapp\.com/
+    }
+  end
 end
+
