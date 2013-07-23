239f1fb4ff3872cdde71da23788b1feb1a5287b4
ruedap <ruedap@ruedap.com>
2013-07-24 06:31:45 +0900

    Fix redirect in rack-rewrite.rb

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -3,7 +3,10 @@ Daplog::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite
 
   if ENV['RACK_ENV'] == 'production'
     r301 %r{.*}, 'http://blog.ruedap.com$&', if: Proc.new {|rack_env|
-      rack_env['SERVER_NAME'] =~ /.+\.herokuapp\.com/
+      rack_env['SERVER_NAME'] =~ /daplog\.herokuapp\.com/
+    }
+    r301 %r{.*}, 'http://blog-staging.ruedap.com$&', if: Proc.new {|rack_env|
+      rack_env['SERVER_NAME'] =~ /daplog-staging\.herokuapp\.com/
     }
   end
 end
