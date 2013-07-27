12c2983313b1dba7bef3685e0dce5e0d5256a3b5
ruedap <ruedap@ruedap.com>
2013-07-27 17:23:16 +0900

    Change raven.rb for excluded_exceptions

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -6,6 +6,9 @@ unless Rails.env.test?
     # 例外通知を行う環境
     config.environments = %w( production staging )
 
+    # 通知から除外する例外
+    config.excluded_exceptions = []
+
     # 例外通知を行わない環境の場合はログの出力を行わない
     unless config.environments.include?(Rails.env)
       config.logger = false
