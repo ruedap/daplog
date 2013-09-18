7707cc78e53d6f234848e5313bfefb2cbaf23373
ruedap <ruedap@ruedap.com>
2013-09-19 03:59:13 +0900

    Change redis url to Redis To Go

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,8 @@
-if ENV['REDISCLOUD_URL']
-  uri = URI.parse(ENV['REDISCLOUD_URL'])
+# redis_url = ENV['REDISCLOUD_URL']
+redis_url = ENV['REDISTOGO_URL']
+
+if redis_url
+  uri = URI.parse(redis_url)
   Redis.current = Redis.new(host: uri.host,
                             port: uri.port,
                             password: uri.password)
