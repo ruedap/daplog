e7bf1eef77a84ee671155c0ca757187649b4427c
ruedap <ruedap@ruedap.com>
2013-09-19 10:59:53 +0900

    Fix redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,9 +2,19 @@ redis_url = ENV['REDISCLOUD_URL']
 
 if redis_url
   uri = URI.parse(redis_url)
-  Redis.current = Redis.new(host: uri.host,
-                            port: uri.port,
-                            password: uri.password)
+  Redis.current = Redis.new(
+    host: uri.host,
+    port: uri.port,
+    password: uri.password
+  )
+  DataMapper.setup(:default, {
+    adapter: 'redis',
+    host: uri.host,
+    port: uri.port,
+    password: uri.password
+  })
 else
   Redis.current = Redis.new(host: 'localhost', port: 6379)
+  DataMapper.setup(:default, { adapter: 'redis' })
 end
+
