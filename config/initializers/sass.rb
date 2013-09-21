5c56e70167d4719db463ea741216ec452cd99b69
ruedap <ruedap@ruedap.com>
2013-09-21 21:11:07 +0900

    Add doc comment in SassCustomFunctions#gradient_count

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -1,4 +1,13 @@
 module SassCustomFunctions
+
+  # Public: 記事一覧のグラデーション表現に必要な数値を返します。
+  # 数値の内容は、データベース内の記事の数と西暦の種類の数を足したものです。
+  # デプロイ時のデータベースの状態から数値を割り出すため、現在の仕様である
+  # デプロイ後にデータベースを再構築する(`rake redis:rebuild`)場合では、
+  # 厳密には正確な数値ではありません。例えば、1記事を追加したデプロイであれば、
+  # デプロイかつ再構築後の総数より、1少ない数値になります。
+  #
+  # 数値をSass::Script::Numberで返します。
   def gradient_count
     articles = Article.all
     size = articles.size
