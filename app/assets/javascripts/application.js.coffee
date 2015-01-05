d8943ebbf3054bf620709e514941529180be3419
ruedap <ruedap@ruedap.com>
2015-01-05 23:13:49 +0900

    Introduce d3.js 3.5.3

--- app/assets/javascripts/application.js.coffee
+++ app/assets/javascripts/application.js.coffee
@@ -1,6 +1,8 @@
 #= require jquery
 #= require jquery_ujs
 #= require vendor/desvg
+#= require vendor/d3
+#= require examples
 
 $ ->
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,3 @@ Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts
 Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf )
 Rails.application.config.assets.precompile += %w( fibonacci-000.css fibonacci-020.css fibonacci-040.css fibonacci-060.css fibonacci-080.css fibonacci-100.css fibonacci-120.css fibonacci-140.css fibonacci-160.css fibonacci-180.css fibonacci-200.css fibonacci-220.css fibonacci-240.css fibonacci-260.css fibonacci-280.css fibonacci-300.css fibonacci-320.css fibonacci-340.css )
 Rails.application.config.assets.precompile += %w( gradient-000.css gradient-020.css gradient-040.css gradient-060.css gradient-080.css gradient-100.css gradient-120.css gradient-140.css gradient-160.css gradient-180.css gradient-200.css gradient-220.css gradient-240.css gradient-260.css gradient-280.css gradient-300.css gradient-320.css gradient-340.css )
-Rails.application.config.assets.precompile += %w( shame-win-chrome.css )
