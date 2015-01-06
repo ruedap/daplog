903e0b03b0a65e5a0fc9036a93bef96d289818fa
ruedap <ruedap@ruedap.com>
2015-01-06 22:23:43 +0900

    Remove set_hue function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   include StagingGuard
   protect_from_forgery with: :exception
-  before_action :set_hue
-
-  private
-
-  def set_hue
-    @hue = "%03d" % (rand(18) * 20)
-  end
 end
