c81502e336b5f7d6ed21f8ae66d9dfc858ec051f
ruedap <ruedap@ruedap.com>
2020-04-13 00:57:52 +0900

    feat(svg): Convert svg to tsx for FooterLogo

--- src/components/atoms/footer_logo/index.tsx
+++ src/components/atoms/footer_logo/index.tsx
@@ -1,14 +1,17 @@
+import SvgFooterLogo from './footer_logo';
 import styled from 'styled-components';
-import logo from './footer-logo.svg';
 
-// TODO: Inline SVG
-const FooterLogo = styled.img.attrs({src: logo, alt: 'logo'})`
-  opacity: 0.6;
-  /* transition: opacity 0.3s;
+// TODO: link
+const FooterLogo = styled(SvgFooterLogo)`
+  .footer_logo_svg__circle {
+    cursor: pointer;
+    fill: rgba(255,255,255,0.55);
+    transition: fill 0.3s;
 
-  &:hover {
-    opacity: 0.8;
-  } */
+    &:hover {
+      fill: rgba(255,255,255,0.89);
+    }
+  }
 `
 
 export default FooterLogo;
