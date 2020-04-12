62aa60255c90f03c1852dd9d65491feda49668a3
ruedap <ruedap@ruedap.com>
2020-04-12 20:27:54 +0900

    feat: Add styles to Header

--- src/components/molecules/header/index.tsx
+++ src/components/molecules/header/index.tsx
@@ -1,10 +1,59 @@
 import React from 'react'
-// import styled from 'styled-components'
+import styled from 'styled-components'
 import HeaderLogo from 'components/atoms/header_logo'
 
-const Header = () => (
-  <HeaderLogo />
-)
+const Inner = styled.div`
+  background-color: #fff;
+`
 
+const Heading = styled.h1`
+  padding-bottom: 11.854%;
+`
+
+const LogoLink = styled.a.attrs({href: '/'})`
+  display: block;
+  position: relative;
+  width: 100%;
+`
+
+const LogoSpacer = styled.div`
+  display: block;
+  font-size: 0;
+  margin-top: -50%;
+  padding-bottom: 100%;
+  position: relative;
+  width: 100%;
+`
+
+const LogoOuter = styled.div`
+  /* TODO: variable */
+  background-color: rgba(24,63,83,0.13);
+  background-image: radial-gradient(rgba(24,63,83,0.13), rgba(24,63,83,0.34));
+  border-radius: 100%;
+  bottom: 0;
+  display: block;
+  height: auto;
+  left: 0;
+  margin: auto;
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 100%;
+`
+
+const Header = () => (
+  <header>
+    <Inner>
+      <Heading>
+        <LogoLink>
+          <LogoSpacer />
+          <LogoOuter>
+            <HeaderLogo />
+          </LogoOuter>
+        </LogoLink>
+      </Heading>
+    </Inner>
+  </header>
+)
 
 export default Header
