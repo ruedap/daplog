928c13229e36b88dc3a003a8f4a4b3535c740279
ruedap <ruedap@ruedap.com>
2020-04-29 22:09:40 +0900

    feat(components): Add useParams

--- src/components/pages/article/index.tsx
+++ src/components/pages/article/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import Header from 'components/molecules/header'
 import Footer from 'components/molecules/footer'
 
-const Article = () => (
+const Article = () => {
+  const { year, month, day, title } = useParams();
+
+  return (
   <>
     <Header />
-    <div>article</div>
+    <div>year: {year}</div>
+    <div>month: {month}</div>
+    <div>day: {day}</div>
+    <div>title: {title}</div>
     <Footer />
   </>
-);
+  )
+}
+;
 
 export default Article;
