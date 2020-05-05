6ccc03e567bec46b8000551a73f63379ea2df935
ruedap <ruedap@ruedap.com>
2020-05-05 17:47:42 +0900

    feat(pages): Implement markdown parsing

--- pages/[id].js
+++ pages/[id].js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Layout from '@components/templates/layout'
 import { getAllArticleIds, getArticleData } from '@utils/articles'
 
@@ -10,7 +11,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const articleData = getArticleData(params.id)
+  const articleData = await getArticleData(params.id)
   return {
     props: {
       articleData
@@ -20,14 +21,19 @@ export async function getStaticProps({ params }) {
 
 export default function Article({ articleData }) {
   return (
-    <Layout>
-      {articleData.title}
-      <br />
-      {articleData.id}
-      <br />
-      {articleData.date}
-      <br />
-
-    </Layout>
+    <>
+      <Head>
+        <title>{articleData.title}</title>
+      </Head>
+      <Layout>
+        {articleData.title}
+        <br />
+        {articleData.id}
+        <br />
+        {articleData.date}
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: articleData.contentHtml }} />
+      </Layout>
+    </>
   )
 }
--- utils/articles.js
+++ utils/articles.js
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
+import remark from 'remark'
+import html from 'remark-html'
 
 const PATH = 'articles'
 const articlesDirectory = path.join(process.cwd(), PATH)
@@ -53,13 +55,19 @@ export function getAllArticleIds() {
   })
 }
 
-export function getArticleData(id) {
+export async function getArticleData(id) {
   const fullPath = path.join(articlesDirectory, `${id}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
   const matterResult = matter(fileContents)
+  
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content)
+  const contentHtml = processedContent.toString()
 
   return {
     id,
+    contentHtml,
     ...matterResult.data
   }
 }
