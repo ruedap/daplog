c078e349017bf6d0f4d8ddae44a2e337c9a2ab2a
ruedap <ruedap@ruedap.com>
2020-05-05 16:33:04 +0900

    feat(pages): Add getStaticProps

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
   webpack(config, options) {
     config.resolve.alias['@components'] = path.join(__dirname, 'components')
+    config.resolve.alias['@lib'] = path.join(__dirname, 'lib')
     config.resolve.alias['@pages'] = path.join(__dirname, 'pages')
     config.resolve.alias['@styles'] = path.join(__dirname, 'styles')
     return config
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { getSortedArticlesData } from '@lib/articles'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allArticlesData = getSortedArticlesData()
+  return {
+    props: {
+      allArticlesData: allArticlesData
+    }
+  }
+}
+
+export default function Home({ allArticlesData }) {
   return (
     <>
       <Head>
@@ -12,40 +22,18 @@ export default function Home() {
         <h1 className="title">
           Welcome to <Link href="/posts/first-post"><a>Next.js!</a></Link>
         </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        
+        <ul className="">
+          {allArticlesData.map(({ id, date, title }) => (
+            <li className="" key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </main>
 
       <footer>
@@ -192,18 +180,7 @@ export default function Home() {
       `}</style>
 
       <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
+      body { background-color: #eee; }
       `}</style>
     </>
   )
