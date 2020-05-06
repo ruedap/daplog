32cc83c418d3411e4933b3418fc7d4c04be69bff
ruedap <ruedap@ruedap.com>
2020-05-06 15:41:08 +0900

    test: Change test code to fail

--- src/tests/greet.test.ts
+++ src/tests/greet.test.ts
@@ -2,7 +2,7 @@ import greet from "@src/greet";
 
 describe('greet', (): void => {
     test('should say hello to Tom.', (): void => {
-        const response: string = greet('Tom');
+        const response: string = greet('Tom_fail');
         expect(response).toBe('Hello, Tom!');
     });
 })
