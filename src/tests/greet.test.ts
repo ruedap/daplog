9606a3a521670d7372d8708aea3e5cdc36ac36ea
ruedap <ruedap@ruedap.com>
2020-05-06 15:45:52 +0900

    Revert "test: Change test code to fail"
    This reverts commit 32cc83c418d3411e4933b3418fc7d4c04be69bff.

--- src/tests/greet.test.ts
+++ src/tests/greet.test.ts
@@ -2,7 +2,7 @@ import greet from "@src/greet";
 
 describe('greet', (): void => {
     test('should say hello to Tom.', (): void => {
-        const response: string = greet('Tom_fail');
+        const response: string = greet('Tom');
         expect(response).toBe('Hello, Tom!');
     });
 })
