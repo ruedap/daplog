073f38bd32f0f7c5cdc7647fc89c0fa9e2fb87e3
ruedap <ruedap@ruedap.com>
2020-05-07 23:51:15 +0900

    feat(styles): Add font styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 import '@src/styles/normalize.scss'
 import '@src/styles/custom_properties.scss'
+import '@src/styles/fonts.scss'
 import '@src/styles/basics.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
