04a2c0cfb2595dc9d4dfddf7f7e81fd9e7c92e31
ruedap <ruedap@ruedap.com>
2020-05-09 17:53:20 +0900

    feat(organisms): Style Article component

--- src/components/organisms/article/index.tsx
+++ src/components/organisms/article/index.tsx
@@ -1,13 +1,31 @@
+import Link from 'next/link'
 import { TArticleData } from '@src/types'
 import Time from '@src/components/atoms/time'
+import styles from './styles.module.scss'
+
+const githubLink = (id: string) => {
+  const baseUrl = 'https://github.com/ruedap/daplog/blob/master/src/articles/'
+  return `${baseUrl}${id}.md`
+}
 
 const Article = ({ articleData }: { articleData: TArticleData }) => {
   return (
-    <>
-      <h1 dangerouslySetInnerHTML={{ __html: articleData.title }} />
-      <Time date={articleData.date} />
-      <div dangerouslySetInnerHTML={{ __html: articleData.body }} />
-    </>
+    <article>
+      <div className={styles.datetimeContainer}>
+        <div className={styles.datetimeSpacer}>
+          <Time date={articleData.date} />
+        </div>
+        {/* TODO: TimeSVG tag */}
+      </div>
+      <h1 className={styles.title} dangerouslySetInnerHTML={{ __html: articleData.title }} />
+      <a className={styles.githubLink} href={githubLink(articleData.id)} />
+      <section className={styles.body} dangerouslySetInnerHTML={{ __html: articleData.body }} />
+      <nav className={styles.nav}>
+        <Link href="/">
+          <a className={styles.navLink}>home</a>
+        </Link>
+      </nav>
+    </article>
   )
 }
 
