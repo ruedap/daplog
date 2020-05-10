a0d4b0935bd1aa72210a373616a92ee174d06902
ruedap <ruedap@ruedap.com>
2020-05-10 23:56:48 +0900

    feat(pages): Add Google Analytics code

---
+++ pages/_document.js
@@ -0,0 +1,37 @@
+// TODO: .ts
+import React from 'react'
+import Document, { Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '@src/utils/gtag'
+
+export default class extends Document {
+  render() {
+    return (
+      // TODO: i18n
+      <html lang="ja">
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
---
+++ src/utils/gtag.ts
@@ -0,0 +1,24 @@
+export const GA_TRACKING_ID = 'UA-39361234-4'
+
+// TODO: Move to .d.ts
+declare global {
+  interface Window {
+    gtag: any
+  }
+}
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = url => {
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  })
+}
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  })
+}
