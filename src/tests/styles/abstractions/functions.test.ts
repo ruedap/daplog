a58d6eed39d7301142e3ba598392008936463e6f
ruedap <ruedap@ruedap.com>
2020-05-12 23:48:36 +0900

    test(styles): Add test for fibo

---
+++ src/tests/styles/abstractions/functions.test.ts
@@ -0,0 +1,23 @@
+import { fibo } from '@src/styles/abstractions/functions'
+
+describe('fibo', () => {
+    test(`should return '1597'.`, () => {
+        const r = fibo('6xl')
+        expect(r).toEqual('1597')
+    });
+
+    test(`should return '55px'.`, () => {
+        const r = fibo('md', 'px')
+        expect(r).toEqual('55px')
+    });
+
+    test(`should return '2rem'.`, () => {
+        const r = fibo('6xs', 'rem')
+        expect(r).toEqual('2rem')
+    });
+
+    test(`should return '1.44'.`, () => {
+        const r = fibo('xl', 'alpha')
+        expect(r).toEqual('1.44')
+    });
+})
