b7ee06dd7c9c2d07024f325e44edcbfc9213e0ea
ruedap <ruedap@ruedap.com>
2020-05-15 02:04:55 +0900

    fix(atoms): Remove loading="lazy"

--- components/atoms/header_logo/index.tsx
+++ components/atoms/header_logo/index.tsx
@@ -1,6 +1,6 @@
 import { BLOG_NAME } from '@/utils/constants'
 
 // TODO: inline svg
-const HeaderLogo = () => <img loading="lazy" src="/images/common/blog-logo.svg" style={{ width: '100% '}} alt={BLOG_NAME} />
+const HeaderLogo = () => <img src="/images/common/blog-logo.svg" style={{ width: '100% '}} alt={BLOG_NAME} />
 
 export default HeaderLogo
