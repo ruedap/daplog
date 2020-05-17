fcc1673eff05975645c58939969d7d79db1f6ae6
ruedap <ruedap@ruedap.com>
2020-05-17 12:01:15 +0900

    feat(atoms): Style FooterLogo component

--- components/atoms/footer_logo/index.tsx
+++ components/atoms/footer_logo/index.tsx
@@ -1,7 +1,17 @@
 import { PORTFOLIO_SITE_NAME } from '@/utils/constants'
-import styles from './styles.module.scss'
 
 // TODO: inline svg
-const FooterLogo = () => <img src="/images/common/footer-logo.svg" className={styles.Root} alt={PORTFOLIO_SITE_NAME} />
+const FooterLogo = () => <Img src="/images/common/footer-logo.svg" alt={PORTFOLIO_SITE_NAME} />
 
 export default FooterLogo
+
+import styled from 'styled-components'
+import Styles from '@/styles'
+
+const Img = styled.img`
+  opacity: ${Styles.funcs.fibo('md', 'alpha')};
+  transition: opacity 0.3s;
+  &:hover {
+    opacity: ${Styles.funcs.fibo('lg', 'alpha')};
+  }
+`
