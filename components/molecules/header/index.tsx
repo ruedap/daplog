f62506491ef1e5d9cdbd77dd250a21b015ff4faa
ruedap <ruedap@ruedap.com>
2020-05-17 12:38:03 +0900

    feat(molecules): Style Header component

--- components/molecules/header/index.tsx
+++ components/molecules/header/index.tsx
@@ -1,24 +1,66 @@
 import Link from 'next/link'
-import styles from './styles.module.scss'
 import HeaderLogo from '@/components/atoms/header_logo'
 
 const Header = () => {
   return (
     <header>
-      <div className={styles.inner}>
-        <div className={styles.heading}>
+      <Inner>
+        <Heading>
           <Link href="/">
-            <a className={styles.logoLink}>
-              <div className={styles.logoSpacer} />
-              <div className={styles.logoOuter}>
+            <LogoLink>
+              <LogoSpacer />
+              <LogoOuter>
                 <HeaderLogo />
-              </div>
-            </a>
+              </LogoOuter>
+            </LogoLink>
           </Link>
-        </div>
-      </div>
+        </Heading>
+      </Inner>
     </header>
   )
 }
 
 export default Header
+
+import styled from 'styled-components'
+import Styles from '@/styles'
+
+const Inner = styled.div`
+  ${Styles.mixins.container}
+  background-color: #fff;
+`
+
+const Heading = styled.div`
+  margin: 0;
+  padding-bottom: 11.854%;
+`
+
+const LogoLink = styled.a`
+  display: block;
+  position: relative;
+  width: 100%;
+`
+
+const LogoSpacer = styled.div`
+  display: block;
+  font-size: 0;
+  margin-top: -50%;
+  padding-bottom: 100%;
+  position: relative;
+  width: 100%;
+`
+
+const LogoOuter = styled.div`
+  background-color: var(--b-bgColor-body);
+  background-image: radial-gradient(var(--b-bgColor-body), var(--b-color-key3));
+  border-radius: 100%;
+  bottom: 0;
+  display: block;
+  height: auto;
+  left: 0;
+  margin: auto;
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 100%;
+`
