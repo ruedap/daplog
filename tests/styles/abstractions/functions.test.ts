13711e1ba6057138abcdf5ddfb962cdca9e087f3
ruedap <ruedap@ruedap.com>
2020-05-17 11:32:55 +0900

    fix(styles): Fix fibo function for rem unit

--- tests/styles/abstractions/functions.test.ts
+++ tests/styles/abstractions/functions.test.ts
@@ -2,22 +2,22 @@ import { fibo } from '@/styles/abstractions/functions'
 
 describe('fibo', () => {
     test(`should return '1597'.`, () => {
-        const r = fibo('xl6')
-        expect(r).toEqual('1597')
+        const actual = fibo('xl6')
+        expect(actual).toEqual('1597')
     });
 
     test(`should return '55px'.`, () => {
-        const r = fibo('md', 'px')
-        expect(r).toEqual('55px')
+        const actual = fibo('md', 'px')
+        expect(actual).toEqual('55px')
     });
 
     test(`should return '2rem'.`, () => {
-        const r = fibo('xs6', 'rem')
-        expect(r).toEqual('2rem')
+        const actual = fibo('xs6', 'rem')
+        expect(actual).toEqual('0.2rem')
     });
 
     test(`should return '1.44'.`, () => {
-        const r = fibo('xl', 'alpha')
-        expect(r).toEqual('1.44')
+        const actual = fibo('xl', 'alpha')
+        expect(actual).toEqual('1.44')
     });
 })
