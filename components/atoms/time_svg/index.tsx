220b7c12bb405486299c51681f759ccff29aeb84
ruedap <ruedap@ruedap.com>
2020-05-17 11:54:42 +0900

    feat(atoms): Style TimeSvg component

--- components/atoms/time_svg/index.tsx
+++ components/atoms/time_svg/index.tsx
@@ -1,14 +1,34 @@
-import styles from './styles.module.scss'
-
 const TimeSvg = ({ date }) => {
   return (
-    <svg className={styles.datetimeSVG} version="1.1" viewBox="0 0 987 610">
-      <circle className={styles.datetimeSVGCircle} cx={173} cy={377} r={116} />
-      <text className={styles.datetimeSVGText} x={173} y={377} textAnchor="middle" dy={7} stroke="none">
+    <Svg version="1.1" viewBox="0 0 987 610">
+      <Circle cx={173} cy={377} r={116} />
+      <Text x={173} y={377} textAnchor="middle" dy={7} stroke="none">
         { date.replace(/-/g, '.') }
-      </text>
-    </svg>
+      </Text>
+    </Svg>
   )
 }
 
 export default TimeSvg
+
+import styled from 'styled-components'
+import Styles from '@/styles'
+
+const Svg = styled.svg`
+  display: block;
+  left: 0;
+  position: absolute;
+  top: 0;
+`
+
+const Circle = styled.circle`
+  fill: rgba(var(--b-rgb-base), ${Styles.funcs.fibo('sm', 'alpha')});
+`
+
+const Text = styled.text`
+  ${Styles.mixins.fontSmoothing()};
+  fill: #fff;
+  font-family: var(--b-fontFamily-fjalla);
+  font-size: ${Styles.funcs.fibo('xs', 'rem')};
+  letter-spacing: 0.1em;
+`
