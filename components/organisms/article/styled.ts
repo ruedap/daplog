3aef9682561e7fbf6baf9ac0e22aacea1a236039
ruedap <ruedap@ruedap.com>
2020-05-24 11:25:22 +0900

    refactor(organisms): Refactor styles of Article

--- components/organisms/article/styled.ts
+++ components/organisms/article/styled.ts
@@ -1,355 +1,118 @@
-import { css } from 'styled-components'
-import { rgba } from 'polished'
+import styled, { css } from 'styled-components'
 import Styles from '@/styles'
+import * as StyledMarkdown from './_styled_markdown'
 
-const fontSize_md = '1.7rem'
-const fontSize_sm = '1.5rem'
-const fontSize_xs = '1.2rem'
+export const TimeContainer = styled.div`
+  display: block;
+  left: 0;
+  margin: auto;
+  max-width: var(--b-maxWidth-container);
+  pointer-events: none;
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 100%;
+`
 
-export const base = css`
+export const TimeSpacer = styled.div`
+  display: block;
+  font-size: 0;
+  padding-bottom: 61.8%;
+  position: relative;
+  width: 100%;
+`
+
+export const Title = styled.h1`
   ${Styles.mixins.fontSmoothing()}
-  font-family: var(--b-fontFamily-lucida);
-  font-size: ${fontSize_md};
-  line-height: 1.8;
+  font-family: var(--b-fontFamily-crimson);
+  font-size: ${Styles.funcs.fibo('xs', 'rem')};
+  font-weight: normal;
+  line-height: 1.618;
+  margin-top: 0;
+  margin-bottom: ${Styles.funcs.fibo('sm', 'px')};
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 877px;
+  text-align: center;
+  width: 90%;
 
   ${Styles.mq.up.md(css`
-    line-height: 2;
+    font-size: ${Styles.funcs.fibo('sm', 'rem')};
+    margin-bottom: ${Styles.funcs.fibo('md', 'px')};
+
+    span::after {
+      content: "\\000a";
+      white-space: pre-line;
+    }
   `)}
 `
 
-export const heading = css`
-  > h2,
-  > h3 {
-    color: rgba(var(--b-rgb-base), ${Styles.funcs.fibo('lg', 'alpha')});
-    font-weight: normal;
-    line-height: var(--b-ratio-golden);
-    text-align: center;
-  }
+export const GitHubLink = styled.a`
+  ${Styles.mixins.articleSectionMark('xs')}
+  height: ${Styles.funcs.fibo('xs', 'px')};
+  margin-bottom: calc(${Styles.funcs.fibo('sm', 'px')} + 10px);
+  width: calc(${Styles.funcs.fibo('xs', 'px')} * 2);
 
-  > h2 {
-    font-size: 2.3rem;
-    margin-bottom: ${Styles.funcs.fibo('sm', 'px')};
-    margin-top: ${Styles.funcs.fibo('xl', 'px')};
-
-    ${Styles.mixins.articleSectionMark('xs', css`
-      &::before,
-      &::after {
-        top: calc((${Styles.funcs.fibo('xl', 'px')} / 2) * -1);
-      }
-    `)}
-  }
-
-  > h3 {
-    font-size: 2rem;
-    margin-bottom: ${Styles.funcs.fibo('xs', 'px')};
-    margin-top: ${Styles.funcs.fibo('md', 'px')};
-  }
+  ${Styles.mq.up.md(css`
+    ${Styles.mixins.articleSectionMark('sm')}
+    height: ${Styles.funcs.fibo('sm', 'px')};
+    margin-bottom: calc(${Styles.funcs.fibo('md', 'px')} + 10px);
+    width: calc(${Styles.funcs.fibo('sm', 'px')} * 2);
+  `)}
 `
 
-export const p = css`
-  > p {
-    margin-bottom: 1em;
-    margin-top: 1em;
-
-    > a {
-      word-wrap: break-word;
-    }
-  }
+export const Body = styled.section`
+  margin: 0 auto;
+  max-width: 699px;
+  width: 90%;
+  ${StyledMarkdown.base}
+  ${StyledMarkdown.heading}
+  ${StyledMarkdown.p}
+  ${StyledMarkdown.img}
+  ${StyledMarkdown.strong}
+  ${StyledMarkdown.list}
+  ${StyledMarkdown.hr}
+  ${StyledMarkdown.figure}
+  ${StyledMarkdown.cite}
+  ${StyledMarkdown.blockquote}
+  ${StyledMarkdown.small}
+  ${StyledMarkdown.footnotes}
+  ${StyledMarkdown.highlight}
+  ${StyledMarkdown.code}
+  ${StyledMarkdown.table}
+  /* extra styles */
+  ${StyledMarkdown.bookCover}
+  ${StyledMarkdown.embed}
+  ${StyledMarkdown.dapicons}
 `
 
-export const img = css`
-  > p {
-    > img,
-    > a > img {
-      display: block;
-      margin: ${Styles.funcs.fibo('sm', 'px')} auto;
-      max-width: 100%;
-    }
-  }
+export const Nav = styled.nav`
+  margin-top: ${Styles.funcs.fibo('xl', 'px')};
+  padding-bottom: ${Styles.funcs.fibo('xl', 'px')};
+
+  ${Styles.mq.up.md(css`
+    margin-top: ${Styles.funcs.fibo('xl2', 'px')};
+    padding-bottom: ${Styles.funcs.fibo('xl2', 'px')};
+  `)}
 `
 
-export const strong = css`
-  strong,
-  em {
-    background-image: linear-gradient(
-      transparent 0,
-      transparent 60%,
-      rgba(var(--b-rgb-base), ${Styles.funcs.fibo('xs2', 'alpha')}) 60%,
-      rgba(var(--b-rgb-base), ${Styles.funcs.fibo('xs2', 'alpha')}) 100%
-    );
-    font-style: normal;
-    font-weight: normal;
-    padding: 0.1em;
-  }
-`
+export const NavLink = (() => {
+  const _hover = `rgba(var(--b-rgb-base), ${Styles.funcs.fibo('md', 'alpha')})`
+  const _active = `rgba(var(--b-rgb-base), ${Styles.funcs.fibo('lg', 'alpha')})`
 
-export const list = css`
-  > ul,
-  > ol {
-    font-size: ${fontSize_sm};
-    margin-bottom: 1em;
-    margin-top: 1em;
-    text-align: left; /* overwrite justify */
-  }
-
-  ul {
-    list-style-type: disc;
-    padding-left: 2.5em;
-  }
-
-  ol {
-    list-style-type: decimal;
-    padding-left: 2.5em;
-  }
-`
-
-export const hr = (() => {
-  const height = Styles.funcs.fibo('md', 'px')
-  return css`
-    > hr {
-      border: 0;
-      display: block;
-      height: ${height};
-      position: relative;
-      width: 100%;
-
-      ${Styles.mixins.articleSectionMark('xs2', css`
-        &::before,
-        &::after {
-          top: calc((${height} / 2) - (${Styles.funcs.fibo('xs2', 'px')} / 2));
-        }
-      `)}
-    }
-`})()
-
-export const figure = css`
-  figure {
-    display: block;
-    margin: 1.5em auto;
-    padding: 0;
-    text-align: center;
-
-    img {
-      display: block;
-      margin: 0 auto;
-      max-width: 100%;
-    }
-
-    figcaption {
-      font-size: ${fontSize_xs};
-      margin: 0.5em 0;
-    }
-  }
-`
-
-const citeIcon = css`
-  &::before {
+  return styled.a`
+    ${Styles.mixins.linkColors('#fff', '#fff', _hover, _hover, _active)}
+    ${Styles.mixins.fontSmoothing()}
     ${Styles.mixins.dapicons}
-    color: rgba(var(--b-rgb-base), ${Styles.funcs.fibo('md', 'alpha')});
-    content: "cite";
-    font-size: 89%;
-    margin-right: 0.6em;
-  }
-`
-
-export const cite = css`
-  > p > cite {
-    ${citeIcon}
+    background-color: rgba(var(--b-rgb-base), ${Styles.funcs.fibo('xs', 'alpha')});
+    border-radius: 100%;
     display: block;
-    font-size: ${fontSize_sm};
-    font-style: normal;
-    font-weight: normal;
-    margin-left: 1em;
-    text-align: left; /* overwrite justify */
-  }
-`
-
-export const blockquote = css`
-  blockquote {
-    border-left: 6px solid rgba(var(--b-rgb-base), ${Styles.funcs.fibo('xs', 'alpha')});
-    color: rgba(var(--b-rgb-base), ${Styles.funcs.fibo('lg', 'alpha')});
-    font-size: ${fontSize_sm};
-    margin: 2em 0;
-    padding: 0 1.5em;
-
-    cite {
-      ${citeIcon}
-      display: block;
-      font-size: 1.3rem;
-      font-style: normal;
-      text-align: right;
-    }
-  }
-`
-
-export const small = css`
-  > p > small,
-  > p > ins {
-    color: rgba(var(--b-rgb-base), ${Styles.funcs.fibo('lg', 'alpha')});
-    display: inline-block;
-    font-size: ${fontSize_sm};
-    line-height: var(--b-ratio-golden);
-    text-align: left; /* overwrite justify */
+    font-size: 2.6rem;
+    width: ${Styles.funcs.fibo('md', 'px')};
+    height: ${Styles.funcs.fibo('md', 'px')};
+    line-height: ${Styles.funcs.fibo('md', 'px')};
+    margin: auto;
+    text-align: center;
     text-decoration: none;
-
-    &::before {
-      content: "※";
-      margin-right: 0.3em;
-    }
-  }
-
-  > p > small {
-    color: rgba(var(--b-color-body), ${Styles.funcs.fibo('md', 'alpha')});
-  }
-`
-
-export const footnotes = css`
-  > .footnotes {
-    color: #777;
-    font-size: ${fontSize_xs};
-    line-height: var(--b-ratio-golden);
-    margin-top: ${Styles.funcs.fibo('md', 'px')};
-
-    > hr {
-      display: none;
-    }
-
-    > ol > li {
-      margin: 0.5em 0;
-    }
-    
-    .footnote-backref {
-      margin-left: 0.3em;
-    }
-  }
-`
-
-export const highlight = css`
-  > pre {
-    ${Styles.mixins.fontSmoothing(false)}
-    background-color: #f8f8f8;
-    border: 3px solid #eee;
-    box-shadow: inset 0 0 0 1px #ccc;
-    padding: 1px;
-    display: block;
-    margin-bottom: 1em;
-    margin-top: 1em;
-    /* overflow: auto;
-    overflow-x: auto;
-    overflow-y: hidden;
-    padding: 13px;
-    white-space: pre;
-    word-wrap: normal; */
-
-    > code {
-      display: block;
-      padding: 0.7em 1.4em 0.9em;
-      font-family: var(--b-fontFamily-mono);
-      font-size: 1.3rem;
-      font-style: normal;
-      font-weight: normal;
-      line-height: var(--b-ratio-golden);
-      overflow-x: auto;
-    }
-  }
-`
-
-export const code = css`
-  *:not(pre) code,
-  kbd {
-    ${Styles.mixins.fontSmoothing(false)}
-    background-color: #f2f2f2;
-    border: 1px solid ${rgba('#808080', 0.4)};
-    border-radius: 2px;
-    font-family: var(--b-fontFamily-mono);
-    font-size: 79%;
-    margin: 0 0.2em;
-    padding: 0 0.4em;
-    vertical-align: 0.1em;
-    white-space: pre;
-    word-wrap: break-word;
-  }
-`
-
-export const table = (() => {
-  const borderColor = `rgba(var(--b-rgb-base), ${Styles.funcs.fibo('xs2', 'alpha')})`
-  return css`
-    table {
-      background-color: #fff;
-      border: 1px solid ${borderColor};
-      border-collapse: separate;
-      border-left: 0;
-      border-spacing: 0;
-      margin: ${Styles.funcs.fibo('sm', 'px')} auto;
-      width: 100%;
-
-      th,
-      td {
-        border-left: 1px solid ${borderColor};
-        border-top: 1px solid ${borderColor};
-        font-size: 1.3rem;
-        padding: 0.5em 1em;
-        text-align: left;
-        vertical-align: top;
-      }
-
-      th {
-        /* TODO: hide header */
-        &:empty {
-          height: 4px;
-          padding: 0;
-        }
-
-        background-color: ${borderColor};
-        font-weight: normal;
-      }
-
-      caption + thead tr:first-child th,
-      caption + tbody tr:first-child th,
-      caption + tbody tr:first-child td,
-      colgroup + thead tr:first-child th,
-      colgroup + tbody tr:first-child th,
-      colgroup + tbody tr:first-child td,
-      thead:first-child tr:first-child th,
-      tbody:first-child tr:first-child th,
-      tbody:first-child tr:first-child td {
-        border-top: 0;
-      }
-    }
-`})()
-
-export const bookCover = css`
-  .e-bookCover {
-    display: block;
-    float: right;
-    padding: 0.5em 0 0.5em 1.5em;
-    text-align: right;
-
-    > img {
-      margin: 0 !important;
-      width: 90px;
-
-      ${Styles.mq.up.md(css`
-        width: 150px;
-      `)}
-    }
-  }
-`
-
-export const embed = css`
-  .e-embed-instagram {
-    display: flex;
-    height: 354px;
-    margin: 0 auto 2em;
-  }
-
-  twitter-widget.twitter-tweet {
-    margin: 1.4em auto !important;
-  }
-`
-
-export const dapicons = css`
-  .e-dapicons {
-    ${Styles.mixins.dapicons}
-  }
-`
+  `
+})()
