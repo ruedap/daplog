35edd91f2df2cd775d4d20fdd90689b026acaade
ruedap <ruedap@ruedap.com>
2020-05-30 14:12:15 +0900

    fix(styles): Fix error: 'Warning: Prop className did not match.'

--- components/organisms/article_list/index.tsx
+++ components/organisms/article_list/index.tsx
@@ -4,6 +4,8 @@ import { TArticleItem } from '@/types'
 import { id2Url } from '@/utils/string'
 import { format, parseISO } from 'date-fns'
 import * as Styled from './styled'
+import styled, { css } from 'styled-components'
+import Styles from '@/styles'
 
 const Time = ({ dateString, className }: { dateString: string, className?: string }) => {
   const d = parseISO(dateString)
@@ -14,9 +16,9 @@ const Time = ({ dateString, className }: { dateString: string, className?: strin
     <time dateTime={d.toISOString()} className={className}>
       <Styled.Year>{year}</Styled.Year>
       <Styled.Dot>.</Styled.Dot>
-      <Styled.Month>{month}</Styled.Month>
+      <StyledMonth>{month}</StyledMonth>
       <Styled.Dot>.</Styled.Dot>
-      <Styled.Date>{date}</Styled.Date>
+      <StyledDate>{date}</StyledDate>
     </time>
   )
 }
@@ -32,9 +34,6 @@ const ArticleList = ({
     beforeYear = year
     return r
   }
-  
-  const StyledTime = Styled.Time(Time)
-  const StyledItemLink = Styled.ItemLink(Styled.Month, Styled.Date, Styled.Title)
 
   return (
     <Styled.Root>
@@ -50,7 +49,7 @@ const ArticleList = ({
               <Link href="[year]/[month]/[date]/[title]" passHref as={url}>
                 <StyledItemLink>
                   <StyledTime dateString={date} />
-                  <Styled.Title>{title}</Styled.Title>
+                  <StyledTitle>{title}</StyledTitle>
                 </StyledItemLink>
               </Link>
             </Styled.Item>
@@ -62,3 +61,79 @@ const ArticleList = ({
 }
 
 export default ArticleList
+
+const StyledTime = styled(Time)`
+  background-color: #fff;
+  display: block;
+  float: left;
+`
+
+/* month, date, title */
+const _StyledItemLineBase = styled.span`
+  height: ${Styles.funcs.fibo('sm', 'px')};
+
+  ${Styles.mq.up.md(css`
+    height: ${Styles.funcs.fibo('md', 'px')};
+  `)}
+`
+
+const StyledTitle = styled(_StyledItemLineBase)`
+  ${Styles.mixins.ellipsis(1)}
+  background-color: rgba(var(--b-rgb-base), 0.42);
+  display: block;
+  padding: 0 21px;
+
+  ${Styles.mq.up.md(css`
+    margin-left: calc(${Styles.funcs.fibo('md', 'px')} * 2);
+  `)}
+`
+
+/* month, date */
+const _StyledItemLineMonthAndDate = styled(_StyledItemLineBase)`
+  display: none;
+  font-family: var(--b-fontFamily-fjalla);
+  letter-spacing: 0.1em;
+  width: ${Styles.funcs.fibo('md', 'px')};
+  text-align: center;
+
+  ${Styles.mq.up.md(css`
+    display: inline-block;
+  `)}
+`
+
+const StyledMonth = styled(_StyledItemLineMonthAndDate)`
+  background-color: rgba(var(--b-rgb-base), 0.34);
+`
+
+const StyledDate = styled(_StyledItemLineMonthAndDate)`
+  background-color: rgba(var(--b-rgb-base), 0.38);
+`
+
+const StyledItemLink = styled.a`
+  ${Styles.mixins.linkColors(
+    '#fff',
+    `rgba(var(--b-rgb-base), ${Styles.funcs.fibo('sm', 'alpha')})`,
+    `rgba(var(--b-rgb-base), ${Styles.funcs.fibo('md', 'alpha')})`,
+    `rgba(var(--b-rgb-base), ${Styles.funcs.fibo('md', 'alpha')})`,
+    `rgba(var(--b-rgb-base), ${Styles.funcs.fibo('lg', 'alpha')})`,
+  )}
+  background-color: #fff;
+  display: block;
+  font-family: var(--b-fontFamily-lucida);
+  text-decoration: none;
+  width: 100%;
+
+  &:hover {
+    ${StyledMonth} {
+      background-color: rgba(var(--b-rgb-base), 0.26);
+    }
+
+    ${StyledDate} {
+      background-color: rgba(var(--b-rgb-base), 0.3);
+    }
+
+    ${StyledTitle} {
+      background-color: rgba(var(--b-rgb-base), 0.34);
+    }
+  }
+`
