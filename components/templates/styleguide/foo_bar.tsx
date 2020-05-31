0622910012212dbab4b4c7314763a968431acae9
ruedap <ruedap@ruedap.com>
2020-05-31 22:56:36 +0900

    refactor(templates): Refactor styles in Styleguide

--- components/templates/styleguide/foo_bar.tsx
+++
@@ -1,5 +0,0 @@
-import * as Styled from './styled_foo_bar'
-
-export const FooBar = () => <Styled.FooBar css={ Styled.fooBar }>This is FooBar</Styled.FooBar>
-
-export const FooBarTag4 = () => <Styled.FooBarTag4>This is FooBarTag4</Styled.FooBarTag4>
