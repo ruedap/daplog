dac597d801f1310ead9c0d8ec887f581c3da58a0
ruedap <ruedap@ruedap.com>
2020-06-07 18:57:50 +0900

    build(next): Add settings for bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-const path = require('path')
-
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
   webpack (config, _) {
     config.resolve.alias['@'] = __dirname
     return config
   }
-}
+})
+
+module.exports = withBundleAnalyzer
