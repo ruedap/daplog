4a55944944aaf5a1db5e08def89d820c704f6480
ruedap <ruedap@ruedap.com>
2020-06-14 21:46:31 +0900

    refactor(styles): Remove unused styles

--- styles/index.ts
+++ styles/index.ts
@@ -1,13 +1,11 @@
 import * as funcs from './abstracts/funcs'
 import * as mixins from './abstracts/mixins'
-import * as mq from './abstracts/mq'
 import CustomProperties from './basics/custom_properties'
 import Elements from './basics/elements'
 
 const Styles = {
   funcs,
   mixins,
-  mq,
   CustomProperties,
   Elements
 } as const
