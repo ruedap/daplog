2d103f32705df30fe4787f34da61667473d1fb5c
ruedap <ruedap@ruedap.com>
2020-06-28 20:49:27 +0900

    refactor(styles): Refactor TimeSvg using suitNames

--- components/atoms/time_svg/index.tsx
+++ components/atoms/time_svg/index.tsx
@@ -1,11 +1,13 @@
 import styled, { css } from 'styled-components'
+import { suitNames } from '@/utils/string'
 import Styles from '@/styles'
 
 const Component = ({ date, className }: { date: string, className?: string }) => {
+  const { element } = suitNames(String(className))
   return (
     <svg version="1.1" viewBox="0 0 987 610" className={ className }>
-      <circle cx={ 173 } cy={ 377 } r={ 116 } className={ `${className}-circle` } />
-      <text x={ 173 } y={ 377 } textAnchor="middle" dy={ 7 } stroke="none" className={ `${className}-text` }>
+      <circle cx={ 173 } cy={ 377 } r={ 116 } className={ element('circle') } />
+      <text x={ 173 } y={ 377 } textAnchor="middle" dy={ 7 } stroke="none" className={ element('text') }>
         { date.replace(/-/g, '.') }
       </text>
     </svg>
