6131748113496f89e7aed49c15506aa18cdbd87d
ruedap <ruedap@ruedap.com>
2025-12-30 14:32:19 +0900

    style: format code

--- components/molecules/header/index.tsx
+++ components/molecules/header/index.tsx
@@ -3,7 +3,7 @@ import { HeaderLogo } from '@/components/atoms/header_logo'
 
 const Header = ({ className }: {className?: string}) => {
   return (
-    <header className={className}>
+    <header className={ className }>
       <div className="max-w-[987px] mx-auto bg-white">
         <div className="m-0 pb-[11.854%]">
           <Link href="/" passHref>
@@ -11,10 +11,10 @@ const Header = ({ className }: {className?: string}) => {
               <div className="block font-[0] -mt-[50%] pb-[100%] relative w-full" />
               <div
                 className="rounded-full bottom-0 block h-auto left-0 m-auto absolute right-0 top-0 w-full"
-                style={{
-                  backgroundImage: `radial-gradient(rgba(24, 63, 83, 0.13), rgba(24, 63, 83, 0.34))`,
-                  backgroundColor: `rgba(24, 63, 83, 0.13)`
-                }}
+                style={ {
+                  backgroundImage: 'radial-gradient(rgba(24, 63, 83, 0.13), rgba(24, 63, 83, 0.34))',
+                  backgroundColor: 'rgba(24, 63, 83, 0.13)'
+                } }
               >
                 <HeaderLogo />
               </div>
