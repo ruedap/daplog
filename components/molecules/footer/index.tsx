c33e1c3a618c54fb428957c2fecc72033371babb
ruedap <ruedap@ruedap.com>
2025-12-30 15:06:38 +0900

    revert: FooterのTailwind CSS移行を取り消し (デザイン崩れのため)

--- components/molecules/footer/index.tsx
+++ components/molecules/footer/index.tsx
@@ -1,15 +1,18 @@
 import { FooterLogo } from '@/components/atoms/footer_logo'
 import { PORTFOLIO_SITE_NAME } from '@/utils/constants'
+import styled, { css } from 'styled-components'
+import Styles from '@/styles'
+import { suitNames, getStyledComponentsClassName } from '@/utils/string'
 
-const Footer = ({ className }: {className?: string}) => {
+const Component = ({ className }: {className?: string}) => {
+  const scClassName = getStyledComponentsClassName(String(className))
+  const { element } = suitNames(scClassName)
   return (
     <footer className={ className }>
-      <div className="max-w-container mx-auto bg-gradient-to-b from-key-3 to-key-4 lg:relative">
-        <div className="m-0 relative lg:before:content-['ruedap'] lg:before:absolute lg:before:text-key-2 lg:before:font-dapicons lg:before:text-md lg:before:top-[377px] lg:before:left-1/2 lg:before:-translate-x-1/2 lg:before:-translate-y-full lg:before:antialiased lg:before:leading-none">
-          <div className="block font-[0] w-full relative pb-[114.387%]">
-            { PORTFOLIO_SITE_NAME }
-          </div>
-          <a href="https://ruedap.com" className="block absolute top-0 bottom-0 left-0 right-0 m-auto h-auto">
+      <div className={ element('container') }>
+        <div className={ element('heading') }>
+          <div className={ element('logoSpacer') }>{ PORTFOLIO_SITE_NAME }</div>
+          <a href="https://ruedap.com" className={ element('logoLink') }>
             <FooterLogo />
           </a>
         </div>
@@ -18,4 +21,58 @@ const Footer = ({ className }: {className?: string}) => {
   )
 }
 
-export { Footer }
+export const StyledComponent = styled(Component)`
+  ${({ theme }) => css`
+    display: block;
+    position: relative;
+  
+    &-container {
+      ${Styles.mixins.container};
+      background-image: linear-gradient(
+        ${theme.colors.key[3]} 0,
+        ${theme.colors.key[4]} 100%
+      );
+
+      ${theme.mq.up.lg} {
+        &::before {
+          ${Styles.mixins.dapicons};
+          color: ${theme.colors.key[2]};
+          content: "ruedap";
+          font-size: ${theme.fontSizes.md.rem};
+          left: 50%;
+          position: absolute;
+          top: 377px;
+          transform: translate(-50%, -100%);
+        }
+      }
+    }
+    
+    &-heading {
+      margin: 0;
+      position: relative;
+    }
+
+    &-logoSpacer {
+      display: block;
+      font-size: 0;
+      /* 1129 / 987 = 1.143870314 */
+      padding-bottom: 114.387%;
+      position: relative;
+      width: 100%;
+    }
+    
+    &-logoLink {
+      bottom: 0;
+      display: block;
+      height: auto;
+      left: 0;
+      margin: auto;
+      position: absolute;
+      right: 0;
+      top: 0;
+    }
+  `}
+  
+`
+
+export const Footer = StyledComponent
